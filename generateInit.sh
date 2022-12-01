46c91a3b04eea66ba43d39bb5df5ef6560f6dc99
iCiaran <iCiaran@users.noreply.github.com>
2022-12-01 12:41:07 +0000

    Format

--- generateInit.sh
+++ generateInit.sh
@@ -8,7 +8,7 @@ for day in $(ls solutions/day_* 2> /dev/null)
 do
     filename=$(basename -- "$day")
     day_number=$(echo "${filename%.*}" | cut -d '_' -f2)
-    echo 'from .day_##DAY## import a as _##DAY##_a,b as _##DAY##_b, parse_input as _##DAY##_parse_input' | sed -e "s/##DAY##/${day_number}/g" >> $INIT_PATH
+    echo 'from .day_##DAY## import a as _##DAY##_a, b as _##DAY##_b, parse_input as _##DAY##_parse_input' | sed -e "s/##DAY##/${day_number}/g" >> $INIT_PATH
 done
 
 echo -e "\ndays = {" >> $INIT_PATH
@@ -18,7 +18,7 @@ do
     filename=$(basename -- "$day")
     day_number=$(echo "${filename%.*}" | cut -d '_' -f2)
     day_number_stripped=$(echo $day_number | awk '{print $1 + 0}')
-    echo "    ##STRIPPED##: {'A': _##DAY##_a, 'B': _##DAY##_b, 'input': _##DAY##_parse_input}," | sed -e "s/##DAY##/${day_number}/g" -e "s/##STRIPPED##/${day_number_stripped}/g" >> $INIT_PATH
+    echo "    ##STRIPPED##: {\"A\": _##DAY##_a, \"B\": _##DAY##_b, \"input\": _##DAY##_parse_input}," | sed -e "s/##DAY##/${day_number}/g" -e "s/##STRIPPED##/${day_number_stripped}/g" >> $INIT_PATH
 done
 
 echo "}" >> $INIT_PATH
